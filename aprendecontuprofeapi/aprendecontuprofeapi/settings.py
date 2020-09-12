7e9fedebeca1b3873742bb8677a1f7c3ee1adfdb
DayanaHerGal06 <dayana.herrera@correounivalle.edu.co>
2020-09-12 09:45:39 -0500

    estudiantes y docentes

--- aprendecontuprofeapi/aprendecontuprofeapi/settings.py
+++ aprendecontuprofeapi/aprendecontuprofeapi/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'aprendecontuprofeapi',
+    'estudiante.apps.EstudianteConfig',
+    'docente.apps.DocenteConfig',
+
 ]
 
 MIDDLEWARE = [
--- aprendecontuprofeapi/docente/models.py
+++ aprendecontuprofeapi/docente/models.py
@@ -2,19 +2,18 @@ from django.db import models
 
 # Create your models here.
 
-class Estudiante(models.Model):
+class Docente(models.Model):
     nombre = models.CharField(max_length=50)
     apellidos = models.CharField(max_length=50)
     tipoIdentificacion = models.CharField(max_length=50)
     numeroIdentificacion = models.IntegerField()
     fechaNacimiento = models.DateField()
     lugarNacimiento = models.CharField(max_length=50)
+    correoElectronco = models.CharField(max_length=50)
     direccionResidencia = models.CharField(max_length=50)
-    nombreCompletoAcudiente = models.CharField(max_length=50)
-    tipoIdentificaionAcudiente = models.CharField(max_length=50)
-    numeroIdentificacionAcudiente = models.IntegerField()
-    numeroTelefonicoAcudiente = models.IntegerField()
-    correoElectroncoAcudiente = models.CharField(max_length=50)
+    numeroCelular = models.IntegerField()
+    numeroTelefonoFijo = models.IntegerField()
     idInicioSesion = models.CharField(max_length=50)
     contrasena = models.CharField(max_length=50)
     urlFotografia = models.CharField(max_length=50)
+
